66041f82ab7347873f85ea26cecd0d99bf9aa364
lalo santoyo <eduardo.santoyo@cceo.com.mx>
2017-12-15 11:15:39 -0600

    inicio

---
+++ app.js
@@ -0,0 +1,7 @@
+function saludar(nombre) {
+    console.log("hola " + nombre.toUpperCase());
+}
+var wolverine = {
+    nombre: "lalo"
+};
+saludar(wolverine.nombre);
---
+++ app.ts
@@ -0,0 +1,8 @@
+function saludar(nombre){
+
+console.log("hola "+ nombre.toUpperCase());
+}
+var wolverine = {
+nombre:"lalo"
+};
+saludar(wolverine.nombre);
